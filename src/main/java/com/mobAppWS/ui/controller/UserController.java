9ea7242a893c8af9db49149d9ade368d90eb8f14
kanha55266442 <39862744+kanha55266442@users.noreply.github.com>
2019-12-12 03:39:33 +0530

    second commit

---
+++ src/main/java/com/mobAppWS/ui/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.mobAppWS.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "post user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+
+}
